b128418971c4ce46facce6018f0635f1635b98fc
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-14 12:01:58 -0600

    Add project files.

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cTemp = TempConverter.FahrenheitToCelsius(68);
+            var fTemp = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine(cTemp);
+            Console.WriteLine(fTemp);
+
+            
+
+
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var fTemp = (fahrenheit - 32) / 1.8;
+            //var ftemp = (fahrenheit - 32) * 9 / 5;
+            return fTemp;
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+           return (celsius * 9) / 5 + 32; 
+        }
+
+    }
+}
